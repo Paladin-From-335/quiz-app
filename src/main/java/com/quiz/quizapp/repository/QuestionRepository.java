1d101a96c834ea58117ca288c1a46bd161879fb4
Andrii Anufriiev <63143166+Paladin-From-335@users.noreply.github.com>
2024-11-07 21:08:54 +0100

    Revert "Quiz UUID n uri generation"

--- src/main/java/com/quiz/quizapp/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quizapp.repository;
-
-import com.quiz.quizapp.model.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
