95ac36f845128ae7ff260cad2b054af611a8fc0a
Paladin-from-335 <andrei.anufriev.work@gmail.com>
2025-03-14 18:16:56 +0100

    name to optionName + mapping update

--- src/main/java/com/quiz/quizapp/model/httpmodel/request/CreateOptionRequest.java
+++ src/main/java/com/quiz/quizapp/model/httpmodel/request/CreateOptionRequest.java
@@ -3,5 +3,5 @@ package com.quiz.quizapp.model.httpmodel.request;
 /**
  * <p>Represents a data of an answer option creation.</p>
  */
-public record CreateOptionRequest(String name, Boolean isCorrect) {
+public record CreateOptionRequest(String optionName, Boolean isCorrect) {
 }
--- src/main/java/com/quiz/quizapp/utils/mapper/OptionMapper.java
+++ src/main/java/com/quiz/quizapp/utils/mapper/OptionMapper.java
@@ -4,17 +4,22 @@ import com.quiz.quizapp.model.entity.Option;
 import com.quiz.quizapp.model.httpmodel.request.CreateOptionRequest;
 import com.quiz.quizapp.model.httpmodel.response.OptionResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public abstract class OptionMapper {
 
+  @Mapping(target = "name", source = "optionName")
   public abstract Option map(CreateOptionRequest source);
 
+  @Mapping(target = "name", source = "optionName")
   public abstract List<Option> map(List<CreateOptionRequest> source);
 
+  @Mapping(source = "name", target = "optionName")
   public abstract OptionResponse mapToHttp(Option source);
 
+  @Mapping(source = "name", target = "optionName")
   public abstract List<OptionResponse> mapToHttp(List<Option> source);
 
 }
